e3ae8f9a5388c288cd27fb5e595028560417d03e
Ricardo Tondello <ricardo.tondello@visma.com>
2023-11-24 17:49:09 +0100

    project init (#1)
    * project init
    
    * remove implicit using

---
+++ src/Functional.Benchmark/IFunctionalBenchmark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Functional.Benchmark;
+
+public interface IFunctionalBenchmark
+{
+    Task<T> BenchmarkAsync<T>(Func<Task<T>> funcTask, Action<ValueStopwatch> actionStopwatch);
+    Task<T> BenchmarkAsync<T>(Func<Task<T>> funcTask, Func<ValueStopwatch, Task> actionStopwatchAsync);
+    Task BenchmarkAsync(Func<Task> funcTask, Action<ValueStopwatch> actionStopwatch);
+    Task BenchmarkAsync(Func<Task> funcTask, Func<ValueStopwatch, Task> actionStopwatchAsync);
+    void Benchmark(Action action, Action<ValueStopwatch> actionStopwatch);
+    void Benchmark(Action action, out ValueStopwatch valueStopwatch);
+    T Benchmark<T>(Func<T> func, Action<ValueStopwatch> actionStopwatch);
+    T Benchmark<T>(Func<T> func, out ValueStopwatch valueStopwatch);
+}
